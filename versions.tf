16ef132d18f5e39536d5df2c62a1b1e74845972c
Dhruv Gupta <dhruv.gupt@tothenew.com>
2025-04-25 12:27:28 +0530

    rds-scheduler

--- versions.tf
+++ versions.tf
@@ -1,3 +1,11 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 1.11.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.95.0"
+    }
+  }
 }
+
